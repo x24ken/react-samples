baa0471ec733d7897c7d029be51282e3d4e5becf
x24ken <x24ken@gmail.com>
2018-05-14 23:17:26 +0000

    first app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var http = require('http');
+var express = require('express');
+
+var app = express()
+
+app.use(function(req, res, next) {
+    return res.send('Hello World');
+});
+
+var server = http.createServer(app);
+
+server.listen('8080');
